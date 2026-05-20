ae16ae75f320c91cdcc3cbafb7084481cb6d787b
Perfectblue1710 <perfectblueee00@gmail.com>
2026-05-20 09:23:11 +0500

    use localStorage fallback

--- frontend/src/hooks/useWebSocket.js
+++ frontend/src/hooks/useWebSocket.js
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { initSocket, closeSocket } from '../services/socket';
+import { initSocket} from '../services/socket';
 
 import {
   addMessage,
